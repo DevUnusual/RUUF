811940faae1dcbf7d1f44d388305bd1eb2f27b4c
DevUnusual <carlos.meneses.g.s@gmail.com>
2026-03-16 11:40:57 -0300

    Initial Commit

---
+++ RUUF/RUUFApp.swift
@@ -0,0 +1,17 @@
+//
+//  RUUFApp.swift
+//  RUUF
+//
+//  Created by Carlos Sousa on 16/03/26.
+//
+
+import SwiftUI
+
+@main
+struct RUUFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
